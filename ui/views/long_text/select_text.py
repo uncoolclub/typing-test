46c8b5ed446a36aa592da9b7a246df345225c4e7
uncoolclub <azxq1000@gmail.com>
2024-06-09 06:19:06 +0900

    refactor: SELECT_TEXTS 상수 추가 및 create_window 메서드 수정

--- ui/views/long_text/select_text.py
+++ ui/views/long_text/select_text.py
@@ -1,7 +1,18 @@
-from tkinter import LabelFrame, Label, Tk, GROOVE, Frame
+from tkinter import LabelFrame, Label, GROOVE, Frame
 from ui.global_font import GlobalFont
 from ui.widgets.tklistbox import TKListbox
-from ui.window.default_window import DefaultWindow
+import customtkinter as ctk
+
+SELECT_TEXTS = [
+    {"label": "하늘과 바람과 별과 시", "file_name": "default2.txt"},
+    {"label": "애국가", "file_name": "default1.txt"},
+    {"label": "동백꽃", "file_name": "default4.txt"},
+    {"label": "별 헤는 밤", "file_name": "default5.txt"},
+    {"label": "부서와 안나귀", "file_name": "default6.txt"},
+    {"label": "성산도", "file_name": "default7.txt"},
+    {"label": "보고싶어", "file_name": "default8.txt"},
+    {"label": "나의 사랑", "file_name": "default9.txt"},
+]
 
 
 class SelectTextWindow:
@@ -9,8 +20,8 @@ class SelectTextWindow:
         self.master = master
         self.create_window(master)
 
-    @staticmethod
-    def create_window(master):
+    def create_window(self, master):
+        from ui.window.default_window import DefaultWindow
         custom_window = DefaultWindow(root=master, master=master, title="글 선택", window_size="720x768")
         frame = custom_window.get_frame()
 
@@ -19,33 +30,35 @@ class SelectTextWindow:
                                        font=font, labelanchor="n")
         select_text_frame.pack(padx=70, pady=10, fill="both", expand=True)
 
-        # Sample items (글 목록)
-        items = [
-            {"label": "패밀꽃 필 무렵", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "마지막 잎새", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "동백꽃", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "별 헤는 밤", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "부서와 안나귀", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "성산도", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "보고싶어", "on_item_selected": lambda label: print(f"Selected: {label}")},
-            {"label": "나의 사랑", "on_item_selected": lambda label: print(f"Selected: {label}")},
-        ]
-
-        custom_listbox = TKListbox(select_text_frame, items)
+        custom_listbox = TKListbox(select_text_frame, SELECT_TEXTS, on_item_selected=self.on_item_selected)
         custom_listbox.create_listbox()
 
         label_frame = Frame(frame, relief=GROOVE, bd=2, bg="#AAAAAA")
         label_frame.pack(padx=5, pady=(20, 5), fill="x", expand=True)
 
         notice_font = GlobalFont.get_global_font(font_size=20)
-        label = Label(label_frame, text="연습하고자 하는 글을 선택하세요", font=notice_font, bg="#AAAAAA", fg="white", anchor="w")
+        label = Label(label_frame, text="연습하고자 하는 글을 선택하세요", font=notice_font, bg="#AAAAAA", fg="#FFFF00", anchor="w")
         label.pack(fill="both")
+
+    def on_item_selected(self, item):
+        self.open_long_text_window(item)
+
+    def open_long_text_window(self, item):
+        self.master.destroy()
+
+        new_root = ctk.CTk()
+        new_root.geometry("1024x768")
+        new_root.configure(fg_color="#AAAAAA")
+
+        from ui.views.long_text.long_text import LongTextWindow
+        LongTextWindow(new_root, file_name=item['file_name'], label=item['label'])
+        new_root.mainloop()
 
 
 if __name__ == "__main__":
-    root = Tk()
+    root = ctk.CTk()
     root.geometry("1024x768")
-    root.configure(bg="#AAAAAA", padx=10)
+    root.configure(fg_color="#AAAAAA", padx=10)
 
     SelectTextWindow(root)
     root.mainloop()
