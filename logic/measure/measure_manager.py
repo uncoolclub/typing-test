27a8ea46977cf2d9129f8cfb2bd9307036178979
youg7297 <youg7297@naver.com>
2024-06-13 02:38:20 +0900

    실시간 정확도 수정

--- logic/measure/measure_manager.py
+++ logic/measure/measure_manager.py
@@ -57,50 +57,38 @@ class MeasureManager:
             "running_time" : self.end_time - self.start_time
         }
 
-
-    def levenshtein_distance(self, s1, s2):
-        if len(s1) < len(s2):
-            return self.levenshtein_distance(s2, s1)
-
-        if len(s2) == 0:
-            return len(s1)
-
-        previous_row = range(len(s2) + 1)
-        for i, c1 in enumerate(s1):
-            current_row = [i + 1]
-            for j, c2 in enumerate(s2):
-                insertions = previous_row[j + 1] + 1
-                deletions = current_row[j] + 1
-                substitutions = previous_row[j] + (c1 != c2)
-                current_row.append(min(insertions, deletions, substitutions))
-            previous_row = current_row
-
-        return previous_row[-1]
-
     def calculate(self, original, comparison):
         if self.next_time == None or len(original) < len(comparison):
             return None, None, None, None
 
         time_taken = time.time() - self.next_time
         self.elapsed_time = time_taken
-        distance = self.levenshtein_distance(original, comparison)
         max_length = max(len(original), len(comparison))
         if max_length == 0:
             return 100.00, 0, 0, 0.0
 
         # 맞은 문자와 틀린 문자 계산
-        matches = sum(1 for o, c in zip(original, comparison) if o == c)
-        mismatches = max_length - matches
+        speed_matches = sum(1 for o, c in zip(original, comparison) if o == c)
+        speed_mismatches = max_length - speed_matches
 
-        accuracy = (1 - distance / max_length) * 100
-        accuracy = round(accuracy, 2)  # 정확도를 소수점 두 자리로 반올림
+        break_hangul_original = break_hangul(original)
+        break_hangul_comparison = break_hangul(comparison)
+        total_matches = sum(1 for i, (o, c) in enumerate(zip(break_hangul_original, break_hangul_comparison)) if i < len(break_hangul_comparison))
+        matches = sum(1 for i, (o, c) in enumerate(zip(break_hangul_original, break_hangul_comparison)) if o == c and i < len(break_hangul_comparison))
+
+        accuracy = 0
+        # accuracy = (1 - distance / max_length) * 100
+        # accuracy = round(accuracy, 2)  # 정확도를 소수점 두 자리로 반올림
+        if total_matches > 0:
+            accuracy = round((matches / total_matches) * 100, 2)
+        else:
+            accuracy = 100.00
 
         # 타자 속도 계산 (초당 입력된 문자 수, KPM 방식)
-        typing_speed = matches / time_taken * 60 if time_taken > 0 else 0
+        typing_speed = speed_matches / time_taken * 60 if time_taken > 0 else 0
         if typing_speed > self.max_typing_speed:
             self.max_typing_speed = typing_speed
 
-        print(self.results)
-        return accuracy, matches, mismatches, typing_speed
+        return accuracy, speed_matches, speed_mismatches, typing_speed
 
 
